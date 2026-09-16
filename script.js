90e8082bf914a22a2b75f23a82dd371ccdc828d3
m1000vrm <mv069411@gmail.com>
2026-09-16 20:45:20 -0300

    Delete script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-let s=588;const el=document.getElementById('timer');setInterval(()=>{if(s<=0)s=588;s--;let m=Math.floor(s/60),x=s%60;el.textContent=String(m).padStart(2,'0')+':'+String(x).padStart(2,'0')},1000);
